7d2061b77d6db97a2bbb28bd0261f56c3716aabe
ahmad_ow <125563646+ahmadowida2512@users.noreply.github.com>
2024-10-22 08:47:56 +0200

    sa.cs

---
+++ sa.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sa : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {  
+        string y ="ahmadddddddddd amakkkkkkkkkkkkkkkk";
+        int x=5;
+        Debug.Log(y);
+        Debug.Log(x);
+        Debug.Log("as");
+        Debug.LogError("hhhhhhhhh There is a problem, loser ");
+                Debug.LogWarning("Try again");
+
+    }
+
+    
+}
